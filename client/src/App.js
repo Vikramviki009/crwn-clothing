8948e189899178bd9770cb59ee5680013057718e
Vikram Dabali <Vikramviki09@gmail.com>
2021-02-02 07:14:32 +0530

    adding performance optimztions to codebase with react lazy, suspense and memo

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import {Route, Switch, Redirect}  from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import Header from './components/header/header.component';
-import CheckoutPage from './pages/checkout/checkout.comoponent';
-import HomePage from './pages/homepage/homepage.components';
-import ShopPage from './pages/shop/shop.components';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import { checkUserSession } from './redux/user/user.actions';
 import { GlobalStyles } from './global.styles';
+import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+const HomePage = lazy(() => import('./pages/homepage/homepage.components'));
+const ShopPage = lazy(() => import('./pages/shop/shop.components'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.comoponent'));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -22,10 +24,14 @@ const App = ({ checkUserSession, currentUser }) => {
         <GlobalStyles/>
         <Header/>
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' render={()=>currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}/>
-          <Route exact path='/checkout' component={CheckoutPage}/>
+          <ErrorBoundary>
+            <Suspense fallback={<Spinner/>}>
+              <Route exact path='/' component={HomePage} />
+              <Route path='/shop' component={ShopPage}/>
+              <Route path='/signin' render={()=>currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}/>
+              <Route exact path='/checkout' component={CheckoutPage}/>
+            </Suspense>
+          </ErrorBoundary>
         </Switch>
       </div>
     );
