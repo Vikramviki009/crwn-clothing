0bf70df9f5fec1bb58d32459c0b9f8e9368a590f
Vikram Dabali <Vikramviki09@gmail.com>
2021-04-19 19:56:01 +0530

    A little correction on server.js file

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 if(process.env.NODE_ENV === 'production'){
+    app.use(compression());
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')))
 
